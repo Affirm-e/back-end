b3bf8f03befff3dc7d9fdde968245a3354425ccb
Briseida Pagador || she/her <bpagador@gmail.com>
2020-09-03 18:41:57 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,14 +8,6 @@ sendRandomTweet()
   .catch((err) => console.log(`could not post tweet`, err))
   .finally(() => mongoose.connection.close());
 
-  
-
-//TECH DEBT:
-// setInterval(sendRandomTweet, 1000*60);
-// ^^ to implement timer on our tweets
-// ensure the tweet being randomly selected is shorter than the character limit for tweets 
-// bad words npm: marriage, husband, wife, dead, names?
-// are there npm packages filtering out BAD PEOPLE? we might be able to filter this by populating the model with the authors we want quotes from 
 
 
 
